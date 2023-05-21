f2a2bd0cb25dadf673f7cf9f35f0f78aeb5571ce
DominikKwiatkowski <domik.rex@gmail.com>
2023-05-21 23:06:27 +0200

    Add ViT.

---
+++ torchreid/models/vit.py
@@ -0,0 +1,302 @@
+"""
+Adapted from 2020 Ross Wightman
+https://github.com/rwightman/pytorch-image-models
+"""
+
+import torch
+import math
+import torch.nn as nn
+import torch.nn.functional as F
+
+from timm.models.layers import DropPath
+from timm.models.layers import trunc_normal_
+from timm.models.vision_transformer import _load_weights
+from timm.models.vision_transformer import default_cfgs
+from timm.models.helpers import load_pretrained, load_custom_pretrained
+
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        trunc_normal_(m.weight, std=0.02)
+        if isinstance(m, nn.Linear) and m.bias is not None:
+            nn.init.constant_(m.bias, 0)
+    elif isinstance(m, nn.LayerNorm):
+        nn.init.constant_(m.bias, 0)
+        nn.init.constant_(m.weight, 1.0)
+
+
+def resize_pos_embed(posemb, grid_old_shape, grid_new_shape, num_extra_tokens):
+    # Rescale the grid of position embeddings when loading from state_dict. Adapted from
+    # https://github.com/google-research/vision_transformer/blob/00883dd691c63a6830751563748663526e811cee/vit_jax/checkpoint.py#L224
+    posemb_tok, posemb_grid = (
+        posemb[:, :num_extra_tokens],
+        posemb[0, num_extra_tokens:],
+    )
+    if grid_old_shape is None:
+        gs_old_h = int(math.sqrt(len(posemb_grid)))
+        gs_old_w = gs_old_h
+    else:
+        gs_old_h, gs_old_w = grid_old_shape
+
+    gs_h, gs_w = grid_new_shape
+    posemb_grid = posemb_grid.reshape(1, gs_old_h, gs_old_w, -1).permute(0, 3, 1, 2)
+    posemb_grid = F.interpolate(posemb_grid, size=(gs_h, gs_w), mode="bilinear")
+    posemb_grid = posemb_grid.permute(0, 2, 3, 1).reshape(1, gs_h * gs_w, -1)
+    posemb = torch.cat([posemb_tok, posemb_grid], dim=1)
+    return posemb
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout, out_dim=None):
+        super().__init__()
+        self.fc1 = nn.Linear(dim, hidden_dim)
+        self.act = nn.GELU()
+        if out_dim is None:
+            out_dim = dim
+        self.fc2 = nn.Linear(hidden_dim, out_dim)
+        self.drop = nn.Dropout(dropout)
+
+    @property
+    def unwrapped(self):
+        return self
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads, dropout):
+        super().__init__()
+        self.heads = heads
+        head_dim = dim // heads
+        self.scale = head_dim ** -0.5
+        self.attn = None
+
+        self.qkv = nn.Linear(dim, dim * 3)
+        self.attn_drop = nn.Dropout(dropout)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(dropout)
+
+    @property
+    def unwrapped(self):
+        return self
+
+    def forward(self, x, mask=None):
+        B, N, C = x.shape
+        qkv = (
+            self.qkv(x)
+            .reshape(B, N, 3, self.heads, C // self.heads)
+            .permute(2, 0, 3, 1, 4)
+        )
+        q, k, v = (
+            qkv[0],
+            qkv[1],
+            qkv[2],
+        )
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+
+        return x, attn
+
+
+class Block(nn.Module):
+    def __init__(self, dim, heads, mlp_dim, dropout, drop_path):
+        super().__init__()
+        self.norm1 = nn.LayerNorm(dim)
+        self.norm2 = nn.LayerNorm(dim)
+        self.attn = Attention(dim, heads, dropout)
+        self.mlp = FeedForward(dim, mlp_dim, dropout)
+        self.drop_path = DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
+
+    def forward(self, x, mask=None, return_attention=False):
+        y, attn = self.attn(self.norm1(x), mask)
+        if return_attention:
+            return attn
+        x = x + self.drop_path(y)
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x
+    
+
+class PatchEmbedding(nn.Module):
+    def __init__(self, image_size, patch_size, embed_dim, channels):
+        super().__init__()
+
+        self.image_size = image_size
+        if image_size[0] % patch_size != 0 or image_size[1] % patch_size != 0:
+            raise ValueError("image dimensions must be divisible by the patch size")
+        self.grid_size = image_size[0] // patch_size, image_size[1] // patch_size
+        self.num_patches = self.grid_size[0] * self.grid_size[1]
+        self.patch_size = patch_size
+
+        self.proj = nn.Conv2d(
+            channels, embed_dim, kernel_size=patch_size, stride=patch_size
+        )
+
+    def forward(self, im):
+        B, C, H, W = im.shape
+        x = self.proj(im).flatten(2).transpose(1, 2)
+        return x
+
+
+class VisionTransformer(nn.Module):
+    def __init__(
+        self,
+        loss,
+        num_classes,
+        image_size,
+        patch_size,
+        n_layers,
+        d_model,
+        n_heads,
+        dropout=0.1,
+        drop_path_rate=0.0,
+        distilled=False,
+        channels=3,
+        **kwargs
+    ):
+        super().__init__()
+        self.d_ff = d_model * 4
+        self.patch_embed = PatchEmbedding(
+            image_size,
+            patch_size,
+            d_model,
+            channels,
+        )
+        self.patch_size = patch_size
+        self.n_layers = n_layers
+        self.d_model = d_model
+        self.n_heads = n_heads
+        self.dropout = nn.Dropout(dropout)
+        self.n_cls = num_classes
+        self.loss = loss
+
+
+        # cls and pos tokens
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, d_model))
+        self.distilled = distilled
+        if self.distilled:
+            self.dist_token = nn.Parameter(torch.zeros(1, 1, d_model))
+            self.pos_embed = nn.Parameter(
+                torch.randn(1, self.patch_embed.num_patches + 2, d_model)
+            )
+            self.head_dist = nn.Linear(d_model, self.n_cls)
+        else:
+            self.pos_embed = nn.Parameter(
+                torch.randn(1, self.patch_embed.num_patches + 1, d_model)
+            )
+
+        # transformer blocks
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, n_layers)]
+        self.blocks = nn.ModuleList(
+            [Block(d_model, n_heads, self.d_ff, dropout, dpr[i]) for i in range(n_layers)]
+        )
+
+        # output head
+        self.norm = nn.LayerNorm(d_model)
+        self.head = nn.Linear(d_model, self.n_cls)
+
+        trunc_normal_(self.pos_embed, std=0.02)
+        trunc_normal_(self.cls_token, std=0.02)
+        if self.distilled:
+            trunc_normal_(self.dist_token, std=0.02)
+        self.pre_logits = nn.Identity()
+
+        self.apply(init_weights)
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {"pos_embed", "cls_token", "dist_token"}
+
+    @torch.jit.ignore()
+    def load_pretrained(self, checkpoint_path, prefix=""):
+        _load_weights(self, checkpoint_path, prefix)
+
+    def featuremaps(self, im):
+        B, _, H, W = im.shape
+        PS = self.patch_size
+
+        x = self.patch_embed(im)
+        cls_tokens = self.cls_token.expand(B, -1, -1)
+        if self.distilled:
+            dist_tokens = self.dist_token.expand(B, -1, -1)
+            x = torch.cat((cls_tokens, dist_tokens, x), dim=1)
+        else:
+            x = torch.cat((cls_tokens, x), dim=1)
+
+        pos_embed = self.pos_embed
+        num_extra_tokens = 1 + self.distilled
+        if x.shape[1] != pos_embed.shape[1]:
+            pos_embed = resize_pos_embed(
+                pos_embed,
+                self.patch_embed.grid_size,
+                (H // PS, W // PS),
+                num_extra_tokens,
+            )
+        x = x + pos_embed
+        x = self.dropout(x)
+
+        for blk in self.blocks:
+            x = blk(x)
+        x = self.norm(x)
+        return x
+
+    def forward(self, im, return_features=False):
+        v = self.featuremaps(im)
+        v = v[:, 0]
+        y = self.head(v)
+        if not self.training:
+            return v
+
+        if self.loss == 'softmax':
+            return y
+        elif self.loss == 'triplet':
+            return y, v
+        else:
+            raise KeyError("Unsupported loss: {}".format(self.loss))
+        
+def ViTSmall(num_classes, loss='softmax', pretrained=True, **kwargs):
+    backbone = "vit_small_patch16_384"
+    default_cfg = default_cfgs[backbone]
+    default_cfg["input_size"] = (3, 128, 256)
+    model = VisionTransformer(
+        num_classes=num_classes,
+        loss=loss,
+        image_size=(128, 256),
+        patch_size=16,
+        n_layers=8,
+        n_heads=8,
+        d_model=384,
+        channels=3,
+    )
+    if pretrained:
+        load_custom_pretrained(model, default_cfg)
+    
+    return model
+
+def ViTMed(num_classes, loss='softmax', pretrained=True, **kwargs):
+    backbone = "vit_base_patch16_384"
+    default_cfg = default_cfgs[backbone]
+    default_cfg["input_size"] = (3, 128, 256)
+    model = VisionTransformer(
+        num_classes=num_classes,
+        loss=loss,
+        image_size=(128, 256),
+        patch_size=16,
+        n_layers=12,
+        n_heads=12,
+        d_model=768,
+        channels=3,
+    )
+    if pretrained:
+        load_custom_pretrained(model, default_cfg)
+    
+    return model
